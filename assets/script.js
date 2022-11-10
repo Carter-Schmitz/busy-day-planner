de8fedae686de11ef105e389e52a97756d109269
Carter_Schmitz <carterschmitz95@gmail.com>
2022-11-09 23:37:44 -0600

    Decided to switch back to creating timeblocks in HTML. Encountered issues with dom traversal

--- assets/script.js
+++ assets/script.js
@@ -8,28 +8,47 @@ $('#currentDay').text(currentDay.format('dddd, MMM Do, YYYY'));
 function updateTime() {
     $('#currentTime').text(currentTime.format('h:mm:ss a'));
 }
-function createSlots() {
-    for (let index = 0; index < workHours.length; index++) {
-        var timeSlot = $('<li>')
-        var hour = $('<p>')
-        var block = $('<p>')
-        var saveBtn = $('<button>')
+// function createSlots() {
+//     for (let index = 0; index < workHours.length; index++) {
+//         var timeSlot = $('<li>')
+//         var hour = $('<p>')
+//         var block = $('<input>')
+//         var saveBtn = $('<button>')
 
-        timeSlot.addClass('row')
-        hour.addClass('hour col-1')
-        block.addClass('block past col-10')
-        saveBtn.addClass('saveBtn col-1')
+//         timeSlot.addClass('row')
+//         hour.addClass('hour col-1')
+//         block.addClass('block past col-10')
+//         saveBtn.addClass('saveBtn col-1')
 
-        hour.text(workHours[index])
+//         hour.text(workHours[index])
 
-        timeBlocks.append(timeSlot)
-        timeSlot.append(hour)
-        timeSlot.append(block)
-        timeSlot.append(saveBtn)
-    }
+//         timeBlocks.append(timeSlot)
+//         timeSlot.append(hour)
+//         timeSlot.append(block)
+//         timeSlot.append(saveBtn)
+//     }
+// };
 
-}
-createSlots();
+$(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".block").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
+})
+
+
+    $("#hour1 .block").val(localStorage.getItem("hour1"));
+    $("#hour2 .block").val(localStorage.getItem("hour2"));
+    $("#hour3 .block").val(localStorage.getItem("hour3"));
+    $("#hour4 .block").val(localStorage.getItem("hour4"));
+    $("#hour5 .block").val(localStorage.getItem("hour5"));
+    $("#hour6 .block").val(localStorage.getItem("hour6"));
+    $("#hour7 .block").val(localStorage.getItem("hour7"));
+    $("#hour8 .block").val(localStorage.getItem("hour8"));
+    $("#hour9 .block").val(localStorage.getItem("hour9"));
+
+// createSlots();
+
 updateTime();
 setInterval(function () {
     updateTime();
